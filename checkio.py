d94b7d67d27d9aa18a716e7456f9c9ae44adb165
David Vargas <davidgiancarlovargassanchez@gmail.com>
2020-06-16 13:39:56 -0500

    Getting links from Island

--- checkio.py
+++ checkio.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
 import time
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
 
 def read_credentials():
@@ -19,6 +21,12 @@ def read_credentials():
         return keys
 
 
+@dataclass
+class Task:
+    name: str
+    link: str
+
+
 class CheckIoSolver:
     """
     1. init is called when object(bot) is created. It initialize the attributes of the class.
@@ -32,6 +40,42 @@ class CheckIoSolver:
         self.base_url = "https://checkio.org"
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
+    def main_login(self):
+        """
+        1. Calls the function to open checkio page
+        2. Keeps it open for 3 seconds
+        3. Close the page (this is used to clean up the dock memory)
+        """
+        self.login_to_checkio()
+        #links = self.get_islands_links
+        self.get_unsolved_tasks_from_island('https://py.checkio.org/station/library/')
+        time.sleep(5)
+        self.driver.quit()
+
+    def solve_tasks_on_island(self, link_to_island):
+        pass
+
+    def get_unsolved_tasks_from_island(self, link_to_island):
+        print('debug')
+        self.driver.get(link_to_island)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        tasks = soup.find_all(class_='island-tasks__container')
+        unsolved_tasks = []
+        for task in tasks:
+            task_status = task.find(class_='island-tasks__side__sign').get('title')
+            if task_status != 'Solved':
+                title = task.find(class_='island-tasks__task__title').get('title')
+                link = task.find('a').get('href')
+                unsolved_tasks.append(Task(title, link))
+        print(unsolved_tasks)
+
+    def get_islands_links(self):
+        opened_stations = self.driver.find_elements_by_xpath("//div[contains(@class,'map__station_state_opened')]")
+        opened_stations_links = []
+        for link in opened_stations:
+            opened_stations_links.append(link.find_element_by_css_selector('a.map__station__link').get_attribute('href'))
+        return opened_stations_links
+
     def login_to_checkio(self):
         """
         1. Opens checkio page using driver variable
@@ -41,7 +85,6 @@ class CheckIoSolver:
         self.driver.get(self.base_url)
         self.get_on_python_checkio()
         self.put_credentials_to_form()
-
 
     def put_credentials_to_form(self):
         """
@@ -53,11 +96,11 @@ class CheckIoSolver:
         """
         try:
             self.driver.find_element_by_id('id_username').send_keys(self.login)
-            time.sleep(1)
+            time.sleep(2)
             password_field = self.driver.find_element_by_id('id_password')
             password_field.send_keys(self.password)
             password_field.submit()
-            time.sleep(1)
+            time.sleep(3)
         except NoSuchElementException:
             print("Exception NoSuchElementException")
 
@@ -71,16 +114,6 @@ class CheckIoSolver:
             time.sleep(2)
         except NoSuchElementException:
             print("incorrect page")
-
-    def main_login(self):
-        """
-        1. Calls the function to open checkio page
-        2. Keeps it open for 3 seconds
-        3. Close the page (this is used to clean up the dock memory)
-        """
-        self.login_to_checkio()
-        time.sleep(3)
-        self.driver.quit()
 
 
 """
@@ -96,8 +129,3 @@ if __name__ == '__main__':
     credentials = read_credentials()
     bot = CheckIoSolver(credentials['username'], credentials['password'])
     bot.main_login()
-
-
-
-
-
